1a69c0a3f714b6fc186b48231f37d8cfe54929b5
Kamtsai <kam3tsai@gmail.com>
2024-09-04 15:52:51 +0800

    Create line_bot_server.py

---
+++ line_bot_server.py
@@ -0,0 +1,29 @@
+from flask import request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from config import LINE_CHANNEL_SECRET, LINE_CHANNEL_ACCESS_TOKEN
+from claude_client import get_claude_response
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+def setup_line_bot(app):
+    @app.route("/callback", methods=['POST'])
+    def callback():
+        signature = request.headers['X-Line-Signature']
+        body = request.get_data(as_text=True)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+        return 'OK'
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        user_message = event.message.text
+        claude_response = get_claude_response(user_message)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=claude_response)
+        )
